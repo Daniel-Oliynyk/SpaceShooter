734ee4bca9f5df18ae1811a8e7cbee9469e2a431
cci18 <cci18@cci18-PC>
2014-11-07 09:03:31 -0500

    Step 1 in changing enemy movement

--- src/spaceshooter/Alien.java
+++ src/spaceshooter/Alien.java
@@ -1,42 +1,63 @@
 package spaceshooter;
 
-import java.awt.Color;
 import java.awt.geom.AffineTransform;
 import static spaceshooter.SpaceShooter.*;
 
 public class Alien extends Sprite {
     static final int SIZE = 80;
-    double angle, speed = 2, targetX, targetY;
+    double angle, speed = ran.nextInt(2) + 2, targetX, targetY, targetAngle;
     int countDown = ran.nextInt(100), health = 3;
-
+    State current = State.Moving, next;
+    
     public Alien(double xp, double yp) {
         x = xp;
         y = yp;
-        angle = Math.atan2(player.y - y, player.x - x);
         targetX = ran.nextInt(WIDTH - SIZE);
         targetY = ran.nextInt(HEIGHT - SIZE);
+        angle = Math.atan2(targetY - (y + (SIZE / 2)), targetX - (x + (SIZE / 2)));
     }
     
     @Override
     void update() {
-        angle = Math.atan2((Player.y + (Player.SIZE / 2)) - (y + (SIZE / 2)), (Player.x + (Player.SIZE / 2)) - (x + (SIZE / 2)));
-        double direction = Math.atan2(targetY - y, targetX - x);
-        if (x > targetX - 5 && x < targetX + 5 && y > targetY - 5 && y < targetY + 5) {
-            speed = 0;
-            countDown--;
-            if (countDown % 30 == 0) bulletBuffer.add(new Projectile((int) (x + (SIZE / 2)), (int) (y + (SIZE / 2)), angle, false));
-            if (countDown < 1) {
-                countDown = ran.nextInt(100);
-                targetX = ran.nextInt(WIDTH - SIZE);
-                targetY = ran.nextInt(HEIGHT - SIZE);
-                speed = 2;
+        if (current == State.Turning) {
+            if (angle < targetAngle) angle += 0.1;
+            else angle -= 0.1;
+            if (angle + 0.05 > targetAngle && angle < targetAngle
+                    && angle + 0.05 > targetAngle && angle < targetAngle)
+                current = next;
+        }
+        else if (current == State.Moving) {
+            x += Math.cos(angle) * speed;
+            y += Math.sin(angle) * speed;
+            if (x > targetX - 5 && x < targetX + 5 && y > targetY - 5 && y < targetY + 5) {
+                speed = ran.nextInt(2) + 2;
+                current = State.Turning;
+                next = State.Shooting;
+                targetAngle = Math.atan2((Player.y + (Player.SIZE / 2)) - (y + (SIZE / 2)),
+                        (Player.x + (Player.SIZE / 2)) - (x + (SIZE / 2)));
             }
         }
-        x += Math.cos(direction) * speed;
-        y += Math.sin(direction) * speed;
+        else if (current == State.Shooting) {
+            angle = Math.atan2((Player.y + (Player.SIZE / 2)) - (y + (SIZE / 2)),
+                    (Player.x + (Player.SIZE / 2)) - (x + (SIZE / 2)));
+            countDown--;
+            if (countDown % 30 == 0) bulletBuffer.add(new Projectile((int) (x + (SIZE / 2)),
+                    (int) (y + (SIZE / 2)), angle, false));
+            if (countDown < 1) {
+                countDown = ran.nextInt(100);
+                speed = ran.nextInt(2) + 2;
+                targetX = ran.nextInt(WIDTH - SIZE);
+                targetY = ran.nextInt(HEIGHT - SIZE);
+                targetAngle = Math.atan2(targetY - (y + (SIZE / 2)), targetX - (x + (SIZE / 2)));
+                current = State.Turning;
+                next = State.Moving;
+            }
+        }
         
-        if (!remove && Player.x + Player.SIZE > x && Player.x < x + SIZE && Player.y + Player.SIZE > y && Player.y < y + SIZE) {
-            explosionBuffer.add(new Explosion((int) x, (int) y, 2, (int) (SIZE * 0.05), Explosion.NO_FRAGMENTS));
+        if (!remove && Player.x + Player.SIZE > x && Player.x < x + SIZE
+                && Player.y + Player.SIZE > y && Player.y < y + SIZE) {
+            explosionBuffer.add(new Explosion((int) x, (int) y, 2, (int) (SIZE * 0.05),
+                    Explosion.NO_FRAGMENTS));
             explosionBuffer.add(new Explosion(Player.x, Player.y, 2, 2, Explosion.NO_FRAGMENTS));
             remove = true;
         }
@@ -45,6 +66,10 @@ public class Alien extends Sprite {
         tran.translate(x, y);
         tran.rotate(angle, SIZE / 2, SIZE / 2);
         painter.drawImage(ImageManager.ENEMY, tran, null);
+    }
+    
+    static enum State {
+        Moving, Turning, Shooting;
     }
     
 }
